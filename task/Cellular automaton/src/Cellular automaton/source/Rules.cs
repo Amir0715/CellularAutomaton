e04950fc764016d76f37375635c6ba7ef9701bf2
Amir Kamolov <kamolov.amir2000@yandex.ru>
2020-10-22 16:57:01 +0300

    Загрузил лр

---
+++ task/Cellular automaton/src/Cellular automaton/source/Rules.cs
@@ -0,0 +1,14 @@
+﻿namespace Cellular_automaton
+{
+    /// <summary>
+    /// Класс предикатов для задания правил показа графически
+    /// </summary>
+    class Rules
+    {
+        public bool Rule_1(Cell cell)
+        {
+            return cell.isAlive;
+        }
+
+    }
+}
