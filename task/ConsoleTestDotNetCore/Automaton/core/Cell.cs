e68071971d413145848223c3486dd67fb3e4db19
Amir <kamolov.amir2000@yandex.ru>
2020-10-29 01:32:13 +0300

    Переехал на юникс

---
+++ task/ConsoleTestDotNetCore/Automaton/core/Cell.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+ namespace Automaton.core
+{
+    public class Cell
+    {
+        public float Value { get; set; }
+        public int NumberOfNeigbors { get; set; }
+        public bool IsAlive { get; set; }
+
+        public void Generate(Random r)
+        {
+            Value = r.Next(1000);
+            IsAlive = r.Next(100) < 50;
+        }
+    }
+}
---
+++ task/ConsoleTestDotNetCore/Automaton/core/Rules.cs
@@ -0,0 +1,14 @@
+﻿namespace Automaton.core
+{
+    /// <summary>
+    /// Класс предикатов для задания правил показа графически
+    /// </summary>
+    class Rules
+    {
+        public bool Rule_1(Cell cell)
+        {
+            return cell.IsAlive;
+        }
+
+    }
+}
